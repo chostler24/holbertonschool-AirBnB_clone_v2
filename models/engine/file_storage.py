02f2897cbd153cd1d893666b5c0cc998b4c53484
GoldLion1011 <yogirose79@gmail.com>
2022-10-25 14:36:50 -0700

    undo filestorage changes :(

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,25 +10,18 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        #if cls is None:
-            #return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
 
-        #cls_objects = {}
+        cls_objects = {}
 
-        #for val in FileStorage.__objects.values():
-            #if type(val) == cls:
-                #cls_objects.update(
-                    #{val.to_dict()['__class__'] + '.' + val.id: val}
-                #)
+        for val in FileStorage.__objects.values():
+            if type(val) == cls:
+                cls_objects.update(
+                    {val.to_dict()['__class__'] + '.' + val.id: val}
+                )
 
-        #return cls_objects
-        if cls in not None:
-            new_dict = {}
-            for key, val in self.__objects.items():
-                if cls == val.__class__:
-                    new_dict[key] = val
-            return new_dict
-        return self.__objects
+        return cls_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -43,13 +36,13 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
-    #def delete(self, obj=None):
-        #"""deletes obj from _objects"""
-        #if obj is None:
-            #return
-        #for key, val in dict(FileStorage.__objects).items():
-            #if val == obj:
-                #del FileStorage.__objects[key]
+    def delete(self, obj=None):
+        """deletes obj from _objects"""
+        if obj is None:
+            return
+        for key, val in dict(FileStorage.__objects).items():
+            if val == obj:
+                del FileStorage.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -74,16 +67,3 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """ Delete object from __objects """
-        if not obj:
-            return
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
-            self.save()
-
-    def close(self):
-        """ call reload() for deserialization """
-        self.reload()
