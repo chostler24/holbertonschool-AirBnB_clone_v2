81d9a33aa319e4fb110af1ecbea33d7c0131cb70
GoldLion1011 <yogirose79@gmail.com>
2022-10-25 11:32:35 -0700

    stuff changed

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class State(BaseModel, Base):
     """ State class """
     name = Column(String(128), nullable=False)
     __tablename__ = 'states'
-    
