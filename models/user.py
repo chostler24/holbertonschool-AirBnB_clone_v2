fe46e669af4bbd556f998cb1d97c8fea534a9c97
chostler24 <connor.hostler@gmail.com>
2022-10-26 10:59:06 -0500

    reverting to check task 7

--- models/user.py
+++ models/user.py
@@ -12,13 +12,13 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship(
-        'Place',
-        backref='user',
-        cascade='delete',
-        passive_deletes=True)
-    reviews = relationship(
-        'Review',
-        backref='user',
-        cascade='all, delete',
-        passive_deletes=True)
+    #places = relationship(
+        #'Place',
+        #backref='user',
+        #cascade='delete',
+        #passive_deletes=True)
+    #reviews = relationship(
+        #'Review',
+        #backref='user',
+        #cascade='all, delete',
+        #passive_deletes=True)
