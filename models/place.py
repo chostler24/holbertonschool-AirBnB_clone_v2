86dd8e11fc3d2bc17feeb606a68f8e52293c592f
chostler24 <connor.hostler@gmail.com>
2022-10-26 11:58:37 -0500

    changed default integer value

--- models/place.py
+++ models/place.py
@@ -14,10 +14,10 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer(0), nullable=False)
-    number_bathrooms = Column(Integer(0), nullable=False)
-    max_guest = Column(Integer(0), nullable=False)
-    price_by_night = Column(Integer(0), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
@@ -27,12 +27,12 @@ class Place(BaseModel, Base):
         backref='state',
         cascade='all, delete-orphan')
 
-    @property
-    def reviews(self):
-        """ getter that returns list of Reviews instances """
-        instances = models.storage.all(Review)
-        new = []
-        for reviews in instances.values():
-            if reviews.place_id == (self.id):
-                new.append(review)
-        return new
+    #@property
+    #def reviews(self):
+        #""" getter that returns list of Reviews instances """
+        #instances = models.storage.all(Review)
+        #new = []
+        #for reviews in instances.values():
+            #if reviews.place_id == (self.id):
+                #new.append(review)
+        #return new
