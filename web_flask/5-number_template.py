6f64f915a874b985162c0e36a156d77bb3a1e2d6
chostler24 <connor.hostler@gmail.com>
2022-11-11 15:51:02 -0600

    Adding task 5 and initial task 6

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""5-number_template.py module"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """method returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """method returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    method returns C, value of text and
+    changes underscore to blank space
+    """
+    text = text.replace("_", " ")
+    return ("C {}".format(text))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """
+    method returns Python, value of text
+    and changes underscore to blank space
+    """
+    text = text.replace("_", " ")
+    return ("Python {}".format(text))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    """
+    method returns n is a number
+    only if n is an integer
+    """
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_temp_route(n):
+    """
+    displays HTML page only if
+    n is integer
+    """
+    return render_template("5-number.html", num=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
