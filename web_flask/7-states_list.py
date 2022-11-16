a62420d1ec9e70e62fc29a8f85e5508d171aa22f
chostler24 <connor.hostler@gmail.com>
2022-11-16 10:29:09 -0600

    task 8 and html file updates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,78 +6,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_route():
-    """method returns Hello HBNB"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb_route():
-    """method returns HBNB"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_route(text):
-    """
-    method returns C, value of text and
-    changes underscore to blank space
-    """
-    text = text.replace("_", " ")
-    return ("C {}".format(text))
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_route(text="is cool"):
-    """
-    method returns Python, value of text
-    and changes underscore to blank space
-    """
-    text = text.replace("_", " ")
-    return ("Python {}".format(text))
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_route(n):
-    """
-    method returns n is a number
-    only if n is an integer
-    """
-    return ("{} is a number".format(n))
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def num_temp_route(n):
-    """
-    displays HTML page only if
-    n is integer
-    """
-    return render_template("5-number.html", num=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def num_odd_even(n):
-    """
-    displays HTML page only if
-    n is an integer
-    """
-    mod = n % 2
-    if mod > 0:
-        num = "{} is odd".format(n)
-    else:
-        num = "{} is even".format(n)
-    return render_template("6-number_odd_or_even.html", num=num)
-
-
 @app.teardown_appcontext
+def teardown(context):
+    """teardown that context"""
+    import models
+    models.storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def state_route():
     """method displays states_list HTML page"""
-    return render_template("7-states_list.html")
+    import models
+    states = models.storage.all(models.state.State)
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
