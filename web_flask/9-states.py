ea05cdfd7afc0a3869da515d696b1e43e9222c9b
chostler24 <connor.hostler@gmail.com>
2022-11-16 14:00:35 -0600

    route file

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,10 +14,26 @@ def teardown(self):
 
 
 @app.route("/states", strict_slashes=False)
-def states():
+def states_route():
     """displays html page"""
+    from models import storage
+    from models.state import State
+    state_list = storage.all(State)
+    return render_template("9-states.html", state_list=state_list)
 
 
 @app.route("/states/<id>", strict_slashes=False)
-def states_id():
+def states_id(id):
     """displays html page"""
+    from models import storage
+    from models.state import State
+    states = storage.all(State)
+    state_list = None
+    for state in states.values():
+        if state.id == id:
+            state_list = state
+    return render_template("9-states.html", state_list=state_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
